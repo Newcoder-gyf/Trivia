3d8709a9d7c4f7ab00934bb0386022cac20075ba
YangfeiGao <gyf112358@hotmail.com>
2018-10-01 18:49:11 -0400

    error handle

--- app/src/main/java/edu/gwu/trivia/activity/GameActivity.kt
+++ app/src/main/java/edu/gwu/trivia/activity/GameActivity.kt
@@ -4,48 +4,47 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import edu.gwu.trivia.BingImageSearchManager
-import edu.gwu.trivia.model.GameData
 import edu.gwu.trivia.R
+import edu.gwu.trivia.model.GameData
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
-
     private val TAG = "GameActivity"
-    private lateinit var gameData: GameData //garrente gamedata will not be null and will init before use it
+    private lateinit var gameData: GameData
     private lateinit var bingImageSearchManager: BingImageSearchManager
 
     private var score = 0
     private var currentQuestionIndex = 0
-    private var numberWrong = 0
+    private var numWrong = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        bingImageSearchManager = BingImageSearchManager(this, imageView = image_view)
+        bingImageSearchManager = BingImageSearchManager(this,image_view)
 
-        val gameData = intent.getParcelableExtra<GameData>(MenuActivity.GAME_DATA_KEY)
-        Log.d(TAG,"we passed game data with ${gameData.question.size} questions")
+        gameData = intent.getParcelableExtra<GameData>(MenuActivity.GAME_DATA_KEY)
+        Log.d(TAG, "we passed game data with ${gameData.question.size} questions")
 
         nextTurn()
-
     }
 
-    private fun nextTurn(){
-        if(numberWrong == 3){ //game over
-            TODO()
+    private fun nextTurn() {
+        if(numWrong == 3 ) { //game over condition
+            //TODO
         }
-        else{
-            currentQuestionIndex ++
+        else {
+            currentQuestionIndex++
             currentQuestionIndex %= gameData.question.size
 
             val question = gameData.question.get(currentQuestionIndex)
             val answer = question.correctAnswer
-            val wrongAnswer = question.wrongAnswer
+            val wrongAnswers = question.wrongAnswer
 
             Log.d(TAG, "the correct answer is ${answer.answer}")
 
             bingImageSearchManager.searchImages(answer.answer)
         }
+
     }
 }
